eccee5e2ad0b4f646e0e24d540c76f266f0cc0c4
Sky <sceleratis@gmail.com>
2021-12-12 11:08:06 -0500

    Added SimplePackage to loader packages
    Should make it so the Packages folder isn't ignored when building.

---
+++ Loader/Config/Packages/SimplePackage/Client/Initializer.lua
@@ -0,0 +1,21 @@
+--[[
+
+	Description: 
+	Author: 
+	Date: 
+
+--]]
+
+
+--// Initializer functions
+return {
+	Init = function(Root, Packages)
+		--// Do client-side init stuff
+		--// print("Hello World! We're in the client init!")
+	end;
+
+	AfterInit = function(Root, Packages)
+		--// Do client-side stuff after init
+		--// print("Hello World! We're in the client after init!")
+	end;
+}
