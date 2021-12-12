d25b512763f860d805035ff6af3d2ba4b82425cd
Sky <sceleratis@gmail.com>
2021-12-12 02:54:27 -0500

    Minor change

--- MainModule/Packages/Core/Client/Modules/Remote.lua
+++ MainModule/Packages/Core/Client/Modules/Remote.lua
@@ -1,5 +1,5 @@
 --[[
-	
+
 	Description: Responsible for basic client-server communication
 	Author: Sceleratis
 	Date: 12/11/2021
@@ -9,7 +9,7 @@
 
 local Package;
 local Utilities;
-local Root; 
+local Root;
 
 local service;
 
@@ -21,7 +21,7 @@ local Sessions = {};
 local RemoteCommands = {
 	SessionData = function(sessionKey, ...)
 		if sessionKey then
-			if Sessions[sessionKey] then 
+			if Sessions[sessionKey] then
 				local session = Root.Remote:GetSession(sessionKey);
 				if session then
 					session:FireEvent(...);
@@ -29,7 +29,7 @@ local RemoteCommands = {
 			end
 		end
 	end;
-	
+
 	LoadCode = function(str, ...)
 		return Root.Bytecode:LoadBytecode(str, nil)(...);
 	end,
@@ -44,7 +44,7 @@ local Methods = {
 				Root.Remote:Send("SessionData", ...);
 			end
 		end,
-		
+
 		FireEvent = function(self, ...)
 			if not self.Ended then
 				self.SessionEvent:Fire(...);
@@ -56,7 +56,7 @@ local Methods = {
 				return self.SessionEvent.Event:Connect(...);
 			end
 		end,
-		
+
 		End = function(self)
 			if not self.Ended then
 				for t,event in next,self.Events do
@@ -67,7 +67,7 @@ local Methods = {
 				self:SendToServer("LeftSession");
 				self.SessionEvent:Destroy();
 				self.Ended = true;
-				
+
 				Sessions[self.SessionKey] = nil;
 			end
 		end;
@@ -99,19 +99,19 @@ local Remote = {
 			return table.unpack(curEvent.RemoteFuncton:InvokeServer(cmd, table.pack(...)));
 		end
 	end,
-	
+
 	GetSession = function(self, sessionKey)
 		if not Sessions[sessionKey] then
 			local session = {
 				SessionKey = sessionKey;
 				SessionEvent = Instance.new("BindableEvent");
-				
+
 				SendToServer = Methods.Session.SendToServer;
 				FireEvent = Methods.Session.FireEvent;
 				Connect = Methods.Session.ConnectEvent;
 				End = Methods.Session.End;
 			}
-			
+
 			session:Connect(function(cmd, ...)
 				if not session.Ended then
 					if cmd == "SessionEnded" then
@@ -119,12 +119,12 @@ local Remote = {
 					end
 				end
 			end)
-			
+
 			session:SendToServer("JoinedSession");
-			
+
 			Sessions[sessionKey] = session;
 		end
-		
+
 		return Sessions[sessionKey];
 	end,
 
@@ -132,7 +132,7 @@ local Remote = {
 	ProcessRemoteCommand = function(self, cmd, args)
 		local cmd = Utilities:Decrypt(cmd, self.SharedKey);
 		local command = self.Commands[cmd];
-		
+
 		Utilities.Events.ReceivedRemoteCommand:Fire(cmd, if type(args) == "table" then table.unpack(args) else args)
 
 		if command then
@@ -148,7 +148,7 @@ local Remote = {
 
 		return self.CurrentEvent;
 	end,
-	
+
 	EventChangeDetected = function(self, c)
 		local curEvent = self.CurrentEvent;
 		if curEvent and not GettingEvent then
@@ -167,15 +167,15 @@ local Remote = {
 	SetupRemote = function(self)
 		if not GettingEvent then
 			GettingEvent = true;
-			
+
 			local foundEvents = false
 			local objBlacklist = {}
-			
+
 			self.CurrentEvent = {}
-			
+
 			--// Find remote event and function
 			repeat
-				
+
 				--// Scan for our RemoteEvent and RemoteFunction
 				for i,child in ipairs(service.ReplicatedStorage:GetChildren()) do
 					if child.Name == self.EventObjectsName and not objBlacklist[child] then
@@ -186,60 +186,56 @@ local Remote = {
 						end
 					end
 				end
-				
+
 				--// Verify that we found the correct event and function (very basic and not-infallible trust checking)
 				if self.CurrentEvent.RemoteFunction and self.CurrentEvent.RemoteEvent then
 					local rawValue = Utilities:RandomString();
 					local encValue = Utilities:Encrypt(rawValue, self.SharedKey);
 					local cmd = Utilities:Encrypt("VerifyRemote", self.SharedKey);
 					local returnedData = nil;
-					
+
 					--// Run it in a pcall, if it errors, it's probably no good
-					local ran,returnedRawData = pcall(function()
-						return self.CurrentEvent.RemoteFunction:InvokeServer(cmd, {rawValue})
+					local ran,returnedRData = pcall(function()
+						return table.unpack(self.CurrentEvent.RemoteFunction:InvokeServer(cmd, {rawValue}))
 					end)
-					
-					if ran and returnedRawData and type(returnedRawData) == "table" then
-						returnedData = table.unpack(returnedRawData);
-					end
-					
+
 					--// If the function returns an event and the event is the event we found then we're good
 					if ran and returnedData and type(returnedData) == "table" and returnedData.Value == encValue and returnedData.Event then
 						local returnedEvent = returnedData.Event;
 						if returnedEvent == self.CurrentEvent.RemoteEvent then
 							foundEvents = true;
-							
+
 						--// If the function returns an event and it's not the one we found, blacklist the one we found for the next scan
 						else
 							objBlacklist[self.CurrentEvent.RemoteEvent] = true;
 						end
-					
+
 					--// If the function did not return an event, then blacklist it as it's probably fake (Malicious server script?)
 					else
 						objBlacklist[self.CurrentEvent.RemoteFunction] = true;
 					end
 				end
-				
+
 			until (foundEvents and self.CurrentEvent.RemoteEvent and self.CurrentEvent.RemoteFunction) or not wait(0.15);
-			
+
 			self.CurrentEvent.RemoteEvent.OnClientEvent:Connect(function(...)
 				self:ProcessRemoteCommand(...);
 			end)
-			
+
 			self.CurrentEvent.RemoteFunction.OnClientInvoke = function(...)
 				return self:ProcessRemoteCommand(...);
 			end
-			
+
 			self.CurrentEvent.RemoteEvent.Changed:Connect(function(...)
 				self:EventChangeDetected(...);
 			end)
-			
+
 			self.CurrentEvent.RemoteFunction.Changed:Connect(function(...)
 				self:EventChangeDetected(...);
 			end)
-			
+
 			Root.DebugWarn("Found Remotes")
-			
+
 			GettingEvent = false;
 		end
 	end,
@@ -264,9 +260,9 @@ return {
 		Remote.EventObjectsName = objNameValue.Value;
 		Remote.SharedKey = sharedKeyValue.Value;
 		Remote:SetupRemote();
-		
+
 		Remote:Send("ClientReady");
-		
+
 		Utilities.Events.ClientReady:Fire();
 	end;
 }
