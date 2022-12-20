47ea4e1af95fcc52d3b0cd77331cbc05d6f04e37
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-12-20 18:05:30 +0200

    Added proper error level support

--- src/MainModule/init.lua
+++ src/MainModule/init.lua
@@ -30,10 +30,10 @@ end
 
 local function error(reason: any?, level: number?)
 	if level ~= nil and type(level) ~= "number" then
-		oError(string.format(":: %s :: bad argument #2 to 'error' (number expected, got %s)", AppName, type(level)))
+		oError(string.format(":: %s :: bad argument #2 to 'error' (number expected, got %s)", AppName, type(level)), 2)
 	end
 
-	oError(":: ".. AppName .." ::"..tostring(reason), level)
+	oError(":: ".. AppName .." ::"..tostring(reason), (level or 1) + 1)
 end
 
 local function print(...)
